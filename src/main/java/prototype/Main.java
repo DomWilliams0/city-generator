9c7d99df1059f9563c85eb858c50ad43ae6e96b3
Dom Williams <me@domwillia.ms>
2017-03-06 10:03:04 +0100

    Merge World and Graph

--- src/main/java/prototype/Main.java
+++ src/main/java/prototype/Main.java
@@ -1,17 +1,18 @@
 package prototype;
 
+import prototype.graph.Graph;
 import prototype.graph.Vertex;
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		World w = new World(1200, 600);
+		Graph graph = new Graph(1200, 600);
 
-		Vertex a = w.addVertex(100, 100);
-		Vertex b = w.addVertex(200, 400);
-		w.addEdge(a, b);
+		Vertex a = graph.addVertex(100, 100, RoadType.MAIN);
+		Vertex b = graph.addVertex(200, 400, RoadType.MAIN);
+		graph.addEdge(a, b);
 
-		w.export();
+		graph.export();
 	}
 }
--- src/main/java/prototype/graph/Graph.java
+++ src/main/java/prototype/graph/Graph.java
@@ -2,31 +2,57 @@ package prototype.graph;
 
 import prototype.RoadType;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 public class Graph
 {
-	private List<Vertex> vertices;
+	private static int NEXT_RENDER = 1;
+
+	private Map<Point2D, Vertex> vertices;
 	private Map<Vertex, Set<Vertex>> edges;
 
-	public Graph()
+	private int width, height;
+
+	public Graph(int width, int height)
 	{
-		vertices = new ArrayList<>();
-		edges = new HashMap<>();
+		this.width = width;
+		this.height = height;
+		this.vertices = new HashMap<>();
+		this.edges = new HashMap<>();
 	}
 
 	public Vertex addVertex(double x, double y, RoadType type)
 	{
-		// TODO return existing vertex ?
+		// bad coords
+		if (!isInRange(x, y))
+			return null;
 
-		Vertex v = new Vertex(x, y, type);
-		vertices.add(v);
+		// fetch existing
+		Point2D.Double point = new Point2D.Double(x, y);
+		Vertex v = vertices.get(point);
+
+		if (v == null)
+		{
+			v = new Vertex(x, y, type);
+			vertices.put(point, v);
+		}
+		else if (v.getType() != type)
+		{
+			System.err.println("Mismatching type of vertex: " + v);
+		}
+
 		return v;
 	}
 
-	public List<Vertex> getVertices()
+	public Collection<Vertex> getVertices()
 	{
-		return vertices;
+		return vertices.values();
 	}
 
 	public Map<Vertex, Set<Vertex>> getEdges()
@@ -46,5 +72,72 @@ public class Graph
 		if (vertices != null)
 			vertices.remove(b);
 
+	}
+	private boolean isInRange(double x, double y)
+	{
+		return x >= 0 && y >= 0 && x < width && y < height;
+	}
+
+	private void render(File outFile)
+	{
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = image.createGraphics();
+
+		// background
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, width, height);
+
+		Stroke MAIN_STROKE = new BasicStroke(1);
+		Stroke MINOR_STROKE = new BasicStroke(1);
+
+		// edges
+		for (Map.Entry<Vertex, Set<Vertex>> e : edges.entrySet())
+		{
+			Vertex v = e.getKey();
+			for (Vertex neighbour : e.getValue())
+			{
+				if (v.isMain() && neighbour.isMain())
+				{
+					g.setColor(Color.BLACK);
+					g.setStroke(MAIN_STROKE);
+				} else
+				{
+					g.setColor(Color.BLUE);
+					g.setStroke(MINOR_STROKE);
+				}
+
+				g.drawLine(v.getIntX(), v.getIntY(), neighbour.getIntX(), neighbour.getIntY());
+			}
+		}
+
+		// vertices
+		g.setColor(Color.RED);
+		g.setStroke(MINOR_STROKE);
+		int rad = 5;
+		for (Vertex v : vertices.values())
+		{
+			g.fillOval(v.getIntX() - rad / 2, v.getIntY() - rad / 2, rad, rad);
+		}
+
+		try
+		{
+			ImageIO.write(image, "png", outFile);
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void export()
+	{
+		File out = new File("/tmp/road-renders/render-" + NEXT_RENDER++ + ".png");
+
+		File parent = out.getParentFile();
+		if (!parent.exists())
+			parent.mkdir();
+
+		out.delete();
+
+		render(out);
 	}
 }
